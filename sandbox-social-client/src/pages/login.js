788c706b082495b5d828a9f3072f85d07062da28
theo <theophilus96@gmail.com>
2021-01-10 12:18:36 +0800

    update

--- sandbox-social-client/src/pages/login.js
+++ sandbox-social-client/src/pages/login.js
@@ -10,29 +10,6 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
-const styles = {
-  form: {
-    textAlign: "center",
-  },
-  image: {
-    margin: "20px auto 20px auto",
-  },
-  pageTitle: {
-    margin: "10px auto 10px auto",
-  },
-  textField: {
-    margin: "10px auto 10px auto",
-  },
-  button: {
-    marginTop: 20,
-    position: "relative",
-  },
-  customError: {
-    color: "red",
-    fontSize: "0.8rem",
-    marginTop: 10,
-  },
-};
 
 const { classes } = this.props;
 
