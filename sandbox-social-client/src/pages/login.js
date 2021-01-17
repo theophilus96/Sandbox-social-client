9cac02e180f0e821d461ccba20eaf2d621a2e8cd
theo <theophilus96@gmail.com>
2021-01-18 00:43:55 +0800

    Update login.js

--- sandbox-social-client/src/pages/login.js
+++ sandbox-social-client/src/pages/login.js
@@ -1,32 +1,37 @@
-import React, { useContext, useState } from "react";
-import withStyles from "@material-ui/core/styles/withStyles";
-import AppIcon from "../images/new sandbox logo.png";
-import { Link } from "react-router-dom";
-import { useForm } from '../util/hooks';
+import React, { Component } from 'react'
+import withStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types';
+import AppIcon from '../images/icon.png';
+import { Link } from 'react-router-dom';
 
-//Material ui
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import { makeStyles } from "@material-ui/styles";
+// MUI Stuff
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+// Redux stuff
+import { connect } from 'react-redux';
+import { loginUser } from '../redux/actions/userActions';
 
-const useStyles = makeStyles((theme) => ({
-...theme
-}));
+const styles = (theme) => ({
+  ...theme
+});
 
-function Login() {
-  const classes = useStyles();
-
-  return (
-    <Grid container className={classes.form}>
-      <Grid item sm />
-      <Grid item sm>
-        <img src={AppIcon} alt="logo" className={classes.image}></img>
-      </Grid>
-    </Grid>
-  );
+export default class login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      errors: {}
+    };
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
-
-export default Login;
