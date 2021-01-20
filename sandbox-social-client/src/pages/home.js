8b2e46ba8651f20c92be3f0602a647cc18a47488
theo <theophilus96@gmail.com>
2021-01-20 21:33:23 +0800

    update to login and home

--- sandbox-social-client/src/pages/home.js
+++ sandbox-social-client/src/pages/home.js
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import PropTypes from "prop-types";
+import axios from "axios";
+import Scream from "../components/Scream";
+import ScreamSkeleton from "../util/ScreamSkeleton";
 
-function home() {
-  return (
-    <div>
-      <h1>home</h1>
-    </div>
-  );
+export class home extends Component {
+  state = {
+    screams: null,
+  };
+  componentDidMount() {
+    axios
+      .get("/screams")
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          screams: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+  render() {
+    const { screams, loading } = this.state;
+
+    let recentScreamsMarkup = screams ? (
+      screams.map((scream) => <Scream key={scream.screamId} scream={scream} />)
+    ) : (
+      <ScreamSkeleton />
+    );
+    return (
+      <Grid container spacing={16}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          {/* <Profile /> */}
+          <h1>profile</h1>
+        </Grid>
+      </Grid>
+    );
+  }
 }
+
+home.propTypes = {
+  getScreams: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired,
+};
 
 export default home;
