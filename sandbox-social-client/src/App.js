2e549c2a24078ec2e5587cb88733712585c19325
theo <theophilus96@gmail.com>
2021-01-31 01:22:09 +0800

    update user page and static profile component

--- sandbox-social-client/src/App.js
+++ sandbox-social-client/src/App.js
@@ -15,6 +15,7 @@ import AuthRoute from "./util/AuthRoute";
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
+import user from "./pages/user";
 import axios from "axios";
 //Redux
 import { Provider } from "react-redux";
@@ -51,6 +52,7 @@ function App() {
                 <Route exact path="/" component={home} />
                 <AuthRoute exact path="/login" component={login} />
                 <AuthRoute exact path="/signup" component={signup} />
+                <Route exact path="/users/:handle" component={user} />
               </Switch>
             </div>
           </Router>
---
+++ sandbox-social-client/src/pages/user.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import Scream from "../components/Scream";
+import StaticProfile from "../components/profile/StaticProfile";
+import Grid from "@material-ui/core/Grid";
+//skeletons
+import ScreamSkeleton from "../util/ScreamSkeleton";
+import ProfileSkeleton from "../util/ProfileSkeleton";
+//redux
+import { connect } from "react-redux";
+import { getUserData } from "../redux/actions/dataActions";
+export class user extends Component {
+  state = {
+    profile: null,
+    postIdParam: null,
+  };
+  componentDidMount() {
+    // const handle = this.props.match.params.handle;
+    // const postId = this.props.match.params.postId;
+
+    const { handle } = this.props.match.params;
+    const { postId } = this.props.match.params;
+
+    if (postId) this.setState({ postIdParam: postId });
+
+    this.props.getUserData(handle);
+    axios
+      .get(`/user/${handle}`)
+      .then((res) => {
+        this.setState({
+          profile: res.data.user,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+  render() {
+    const { screams, loading } = this.props.data;
+    const { postIdParam } = this.state;
+
+    const screamsMarkup = loading ? (
+      <ScreamSkeleton />
+    ) : screams === null ? (
+      <p>No screams from this user</p>
+    ) : !postIdParam ? (
+      screams.map((scream) => <Scream key={scream.postId} scream={scream} />)
+    ) : (
+      screams.map((scream) => {
+        if (scream.postId !== postIdParam)
+          return <Scream key={scream.postId} scream={scream} />;
+        else return <Scream key={scream.postId} scream={scream} openDialog />;
+      })
+    );
+
+    return (
+      <Grid container spacing={16}>
+        <Grid item sm={8} xs={12}>
+          {screamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          {this.state.profile === null ? (
+            <ProfileSkeleton />
+          ) : (
+            <StaticProfile profile={this.state.profile} />
+          )}
+        </Grid>
+      </Grid>
+    );
+  }
+}
+user.propTypes = {
+  getUserData: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  data: state.data,
+});
+
+export default connect(mapStateToProps, { getUserData })(user);
